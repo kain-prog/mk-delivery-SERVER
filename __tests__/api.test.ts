3fbaf918e03101d22fa59534459f676d9af12df0
Kain <mtheusmss@gmail.com>
2023-08-11 20:05:27 -0300

    feature: implement tests

---
+++ __tests__/api.test.ts
@@ -0,0 +1,6 @@
+import { before, it, after, describe  } from 'node:test'; 
+
+
+describe('API E2E Test Suíte', () => {
+    
+})
--- server.ts
+++ server.ts
@@ -7,6 +7,8 @@ app.use('/api', require('./src/routes/productRoute'));
 app.use('/api', require('./src/routes/userRoute'));
 app.use('/api', require('./src/routes/authRoute'));
 
-app.listen(port, () => {
-    console.log(`Server Running on port ${port}`);
-})
+if(process.env.NODE_ENV !== 'test'){
+    app.listen(port, () => {
+        console.log(`Server Running on port ${port}`);
+    })
+}
